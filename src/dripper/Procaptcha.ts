56f99639d4248cf123b08002b603463b0557ddaf
hugh <hugh@prosopo.io>
2023-10-24 08:43:24 +0100

    Removing bundle and adding back regular tests

--- src/dripper/Procaptcha.ts
+++ src/dripper/Procaptcha.ts
@@ -1,7 +1,6 @@
 import { getServerConfig, ProsopoServer } from "@prosopo/server";
 import { ProcaptchaOutput } from "@prosopo/types";
 
-import { ApiPromise } from "@polkadot/api";
 import { logger } from "../logger";
 
 export class Procaptcha {
@@ -10,23 +9,15 @@ export class Procaptcha {
       const prosopoOutput = JSON.parse(captcha) as ProcaptchaOutput;
       const config = getServerConfig();
 
+      console.log(config, "config \n\n\n config -----------------");
+
       const prosopoServer = new ProsopoServer(config);
-      console.log(prosopoServer.wsProvider.endpoint);
 
-      const test1 = await ApiPromise.create({ provider: prosopoServer.wsProvider });
-      // console.log(test1);
-      // const test2 = await prosopoServer.getSigner();
-      // const test3 = await prosopoServer.getContractApi();
+      await prosopoServer.isReady();
 
-      // await prosopoServer.isReady();
-
-      // await prosopoServer.getApi().rpc.chain.getBlockHash(prosopoOutput.blockNumber);
-
-      // console.log("ready -------------- \n\n\n\n\n\0-0-0-0-09-09-09-09-09", prosopoServer.network.endpoint);
-
-      // const captchaResult = await prosopoServer.isVerified(prosopoOutput);
-      // if (captchaResult) return true;
-      // logger.debug("Negative procaptcha validation result", captchaResult);
+      const captchaResult = await prosopoServer.isVerified(prosopoOutput);
+      if (captchaResult) return true;
+      logger.debug("Negative procaptcha validation result", captchaResult);
       return false;
     } catch (e) {
       logger.error(`⭕ An error occurred when validating captcha`, e);
